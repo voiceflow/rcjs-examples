b21b6acc794a9b39efc0d3ccf780eba02b27c179
Zhi Heng (Brennan) Lu <32404412+zhihil@users.noreply.github.com>
2021-02-09 15:55:40 -0500

    feat: adding trace processor example (#2)
    Co-authored-by: Zhiheng Lu <brennan@voiceflow.com>

---
+++ trace-processor/index.js
@@ -0,0 +1,46 @@
+const RuntimeClient = require("@voiceflow/runtime-client-js").default;
+const { makeTraceProcessor } = require("@voiceflow/runtime-client-js");
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // $TODO - Remove this
+
+const printGlobal = (text) => {
+    console.log(`GLOBAL: ${text}`);
+};
+
+const makeLocalString = (text) => {
+    return `LOCAL: ${text}`;
+};
+
+const globalTraceProcessor = makeTraceProcessor({
+    speak: (message) => printGlobal(`SPEAK: ${message}`),
+    debug: (message) => printGlobal(`DEBUG: ${message}`),
+    block: (blockId) => printGlobal(`BLOCK: Entering block ${blockId}`),
+    flow: (diagramID) => printGlobal(`FLOW: Entering ${diagramID}`)
+});
+
+const localTraceProcessor = makeTraceProcessor({
+    speak: (message) => makeLocalString(`SPEAK: ${message}`),
+    debug: (message) => makeLocalString(`DEBUG: ${message}`),
+    block: (blockId) => makeLocalString(`BLOCK: Entering block ${blockId}`),
+    flow: (diagramID) => makeLocalString(`FLOW: Entering ${diagramID}`)
+});
+
+// Construct a new Voiceflow app instance
+const app = new RuntimeClient({
+    versionID: 'XXXXXXXXXXXXXXXXXXXXXXXXX',
+    dataConfig: {
+        includeTypes: ['speak', 'debug', 'block', 'flow'],
+        traceProcessor: globalTraceProcessor
+    }
+});
+
+(async () => {
+    // Start the conversation session and get the initial app response
+    console.log(">>> Running the global trace processor");
+    const response = await app.start();
+
+    console.log(">>> Running the local trace processor");
+    const traces = response.getResponse();
+    const mappedTraces = traces.map(localTraceProcessor);
+    console.log(mappedTraces);
+})();
