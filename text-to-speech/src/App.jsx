e0db83ec5452eda150db0cd9033054bddd5e4e15
Zhiheng Lu <brennan@voiceflow.com>
2021-02-09 15:38:31 -0500

    feat: added TTS demo

--- text-to-speech/src/App.jsx
+++ text-to-speech/src/App.jsx
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RuntimeClient from "@voiceflow/runtime-client-js";
 
 function App() {
+  const chatbot = React.useMemo(() => new RuntimeClient({
+    versionID: 'XXXXXXXXXXXXXXXXXXXXXXX',
+    dataConfig: {
+      tts: true
+    }
+  }), []);
+
+  const [isConvoOver, setConvoOver] = React.useState(true);
+  const [traces, setTraces] = React.useState([]);
+  const inputRef = React.useRef(null);
+
+  const setContext = React.useCallback((context) => {
+    setTraces(context.getResponse());
+    setConvoOver(context.isEnding());
+  }, []);
+
+  const clearUI = React.useCallback(() => {
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+  }, [inputRef]);
+
+  const onInteract = React.useCallback(async () => {
+    const context = isConvoOver 
+      ? await chatbot.start()
+      : await chatbot.sendText(inputRef.current.value ?? '');
+    
+    setContext(context);
+    clearUI();
+  }, [chatbot, inputRef, isConvoOver, setContext, clearUI]);
+
+  const createOnPlayAudio = React.useCallback((audioSrc) => {
+    return () => {
+      const audio = new Audio(audioSrc);
+      audio.play();
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Input:</label><input type="text" ref={inputRef}></input>
+      <button onClick={onInteract}>{ isConvoOver ? 'Start' : 'Send' }</button>
+      <h2>Dialogue</h2>
+      <ul>
+        {
+          traces.map(({ payload: { message, src }}) => (
+            <button 
+              onClick={createOnPlayAudio(src)}
+              style={{ display: 'block' }}
+            >
+                {message}
+            </button> 
+          ))
+        }
+      </ul>
     </div>
   );
 }
