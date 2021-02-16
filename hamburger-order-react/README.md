b0f1f268f742bf98a91fe25dc75ef5c6af6d7f74
Zhi Heng (Brennan) Lu <32404412+zhihil@users.noreply.github.com>
2021-02-16 15:32:44 -0500

    feat: added react demo (#3)
    * feat: added react demo
    
    * docs: adding more documentation
    
    Co-authored-by: Zhiheng Lu <brennan@voiceflow.com>

---
+++ hamburger-order-react/README.md
@@ -0,0 +1,9 @@
+# Hamburger Order React Sample
+
+## Running the Sample
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). You can run the project using `yarn start`
+
+## Explanation
+
+This is a minimal working example that demonstrates how to use the Voiceflow Runtime Client on a React project. 
---
+++ hamburger-order-react/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import RuntimeClient from "@voiceflow/runtime-client-js";
+
+function App() {
+  const [isEnd, setIsEnd] = React.useState(true);   // track whether the conversation has ended
+  const [traces, setTraces] = React.useState([]);   // stores the current response from the VF app
+  const ref = React.useRef(null);
+
+  // Create a `RuntimeClient` instance to connect with your Voiceflow app.
+  const chatbot = React.useMemo(() => new RuntimeClient({
+    versionID: '602beb117504b8001c22b6ae'
+  }), []);
+
+  // Main workhorse logic
+  const handleSend = React.useCallback(async () => {
+    // Get the user's response to the VF app's dialogue
+    const userInput = ref.current.value;
+
+    // Call an Interaction method to start a conversation or advance a conversation based on user input.
+    const context = isEnd ? await chatbot.start() : await chatbot.sendText(userInput);
+  
+    // Store the results of the interaction and then add the data in your JSX to render the response.
+    setTraces(context.getResponse());
+    setIsEnd(context.isEnding());
+  }, [chatbot, isEnd])
+
+  return (
+    <>
+      <input ref={ref} type="text" placeholder="Enter your response..." />
+      <button type="button" onClick={handleSend}>{isEnd ? 'Start' : 'Send'}</button>
+      <ul>
+        {
+          traces.map(trace => <li>{trace.payload.message}</li>) // displays the VF App response
+        }
+      </ul>
+    </>
+  );
+}
+
+export default App;
